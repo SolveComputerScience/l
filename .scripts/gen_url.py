eddd22335c0fe234b6161cf2be30cf7fce5134c3
Franco Masotti <franco.masotti@tutanota.com>
2025-06-02 18:13:50 +0200

    Improve script
    - Use argparse
    - Update links

--- .scripts/gen_url.py
+++ .scripts/gen_url.py
@@ -11,10 +11,19 @@ import pathlib
 import sys
 import qrcode
 from urllib.parse import urlparse
+import argparse
+import slugify
 
 
-URL_SUFFIX='https://SolveComputerScience.github.io/l'
-DST_DIR='../.'
+URL_SUFFIX: str = 'https://SolveComputerScience.github.io/l'
+DST_DIR: str = '../.'
+PAGE_TIMEOUT: int = 2
+UMAMI_BASE_CODE: str = 'src="https://umami.franco.net.eu.org/script.js" data-website-id="763262f8-2ecc-4cec-adae-9318f97128c9"'
+DEFAULT_DATA_TAG: str = 'SolveComputerScience'
+
+
+def gen_umami_code(data_tag: str) -> str:
+    return f'''<script defer {UMAMI_BASE_CODE} data-tag="{data_tag}"></script>'''
 
 
 def check_input(url: str) -> bool:
@@ -25,6 +34,13 @@ def check_input(url: str) -> bool:
         is_url = True
 
     return is_url
+
+
+def url_type(url: str) -> str:
+    if not check_input(url):
+        raise argparse.ArgumentTypeError(f'invalid URL: "{url}", please provide a valid URL starting with https://')
+    return url
+
 
 def get_file_name(hash_hex: str, file_type: str, existing_file: str = '') -> str:
     file: str
@@ -37,7 +53,7 @@ def get_file_name(hash_hex: str, file_type: str, existing_file: str = '') -> str
     return file
 
 
-def create_redirect_file(original_url: str, existing_file: str = '') -> str:
+def create_redirect_file(original_url: str, page_title: str = '', existing_file: str = '') -> str:
     hash_hex: str
     if existing_file:
         # We need to get the exising filename to be used as hash.
@@ -47,7 +63,7 @@ def create_redirect_file(original_url: str, existing_file: str = '') -> str:
 
     file: str = get_file_name(hash_hex, 'html', existing_file)
     
-    html_content: str = f"""<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url={original_url}" /></head><body><p>This redirection page is managed by <a href="https://www.youtube.com/@SolveComputerScience">SolveComputerScience</a></p><p>If you are not redirected, <a href="{original_url}">click here</a>.</p></body></html>"""
+    html_content: str = f"""<!DOCTYPE html><html><head><title>{page_title}</title>{gen_umami_code(slugify.slugify(page_title))}<meta http-equiv="refresh" content="{PAGE_TIMEOUT}; url={original_url}" /></head><body><p>This redirection page is managed by <a href="https://www.youtube.com/@SolveComputerScience">SolveComputerScience</a></p><p>If you are not redirected in {PAGE_TIMEOUT} seconds, <a href="{original_url}">click here</a>.</p></body></html>"""
     
     pathlib.Path(DST_DIR, file).write_text(html_content)
 
@@ -75,35 +91,33 @@ def create_qr(hash_hex: str, existing_file: str = '') -> tuple[str, str]:
     return hash_hex, file
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Manage redirects.')
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # Create command.
+    create_parser = subparsers.add_parser('create', help='Create a new redirect file')
+    create_parser.add_argument('url', type=url_type, help='The URL to create')
+    create_parser.add_argument('title', type=str, help='The title for the URL')
+
+    # Update command.
+    update_parser = subparsers.add_parser('update', help='Update an existing redirect file')
+    update_parser.add_argument('url', type=url_type, help='The URL to update')
+    update_parser.add_argument('file', type=str, help='The file associated with the update')
+    update_parser.add_argument('--title', type=str, help='The new title for the URL', default=DEFAULT_DATA_TAG)
+
+    args = parser.parse_args()
+
+    if args.command == 'create':
+        print(f'Creating entry: URL={args.url}, TITLE={args.title}')
+        hash_hex, html_file = create_redirect_file(args.url, args.title)
+        _, qr_file = create_qr(hash_hex)
+    elif args.command == 'update':
+        print(f'Updating entry: URL={args.url}, TITLE={args.title}, FILE={args.file}')
+        existing_file: str = args.file
+        hash_hex, html_file = create_redirect_file(args.url, args.title, existing_file)
+        _, qr_file = create_qr(hash_hex, existing_file)
+
+
 if __name__ == '__main__':
-    ok: bool = False
-    usage: bool = False
-
-    if len(sys.argv) == 3:
-        # Update.
-        url_new: str = sys.argv[1]
-        if check_input(url_new):
-            existing_file: str = sys.argv[2]
-            hash_hex, html_file = create_redirect_file(url_new, existing_file)
-            _, qr_file = create_qr(hash_hex, existing_file)
-            ok = True
-
-    elif len(sys.argv) == 2:
-        # Create.
-        url: str = sys.argv[1]
-        if check_input(url):
-            hash_hex, html_file = create_redirect_file(url)
-            _, qr_file = create_qr(hash_hex)
-            ok = True
-
-    else:
-        usage = True
-        print('python -m gen_short_link URL')
-        print('python -m gen_short_link URL EXISTING_FILE')
-
-    if not usage:
-        if ok:
-            print(f'{html_file}, {qr_file}')
-        else:
-            print('input is not a valid URL')
-
+    main()
